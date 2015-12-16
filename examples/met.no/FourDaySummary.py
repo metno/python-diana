e9d51961c3f5b539d75df43d0a1415324f46d7b5
David Boddie <david.boddie@met.no>
2015-12-16 16:46:08 +0100

    Added some examples. Updated the version number.

---
+++ examples/met.no/FourDaySummary.py
@@ -0,0 +1,77 @@
+# Created from FourDaySummary.ipynb
+from metno import bdiana, plotcommands
+import pyproj
+from PyQt4.QtCore import QRect, Qt
+from PyQt4.QtGui import QColor, QFont, QImage, QPainter
+
+b = bdiana.BDiana()
+
+b.setup("/etc/diana/setup/diana.setup-COMMON")
+
+model = "EC.GEO.0.25.00"
+field = "accumprecip.24h"
+times = b.getFieldTimes(model, field)
+
+plot_times = []
+for t in times:
+    if t.hour == 6:
+        plot_times.append(t)
+        if len(plot_times) == 4:
+            break
+
+p = pyproj.Proj(proj="stere", datum="WGS84")
+x1, y1 = p(4, 58)
+x2, y2 = p(15, 65)
+
+a = plotcommands.Area(proj4string = p.srs, rectangle = (x1, x2, y1, y2))
+m = plotcommands.Map()
+m.setOption("map", "Gshhs-Auto")
+m.setOption("backcolour", "white")
+m.setOption("land", "on")
+m.setOption("land.colour", "0:64:0")
+
+f = plotcommands.Field(model = model, plot = field, plottype = "contour")
+f.setOption("palettecolours", "wms.precip1h.palett")
+f.setOption("line.values", [0.5,1,2,4,6,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,245,250,255,260,265,270,275,280,285,290,295,300])
+f.setOption("colour", "off")
+f.setOption("table", 1)
+f.setOption("extreme.type", "Value")
+
+b.setPlotCommands([a, m, f])
+
+images = []
+for t in plot_times:
+  b.setPlotTime(t)
+  images.append((t, b.plotImage(150, 300)))
+  
+
+l = plotcommands.Label()
+l.setOption("anno", "<table,fcolour=white>")
+l.setOption("fontsize", 12)
+l.setOption("polystyle", "none")
+l.setOption("halign", "right")
+l.setOption("valign", "top")
+
+b.setPlotCommands([f, l])
+b.setPlotTime(t)
+
+anno = QImage(150, 300, QImage.Format_RGB16)
+anno.fill(QColor(Qt.white))
+b._plot(150, 300, anno, plot_method = b.controller.plotAnnotations)
+
+image = QImage((len(plot_times) * (150 + 25)) + 150, 330, QImage.Format_RGB16)
+image.fill(QColor(Qt.white))
+font = QFont()
+font.setPixelSize(20)
+painter = QPainter()
+painter.begin(image)
+painter.setFont(font)
+x = 0
+for t, im in images:
+    painter.drawImage(x, 0, im)
+    painter.drawText(QRect(x, 305, 150, 25), Qt.AlignCenter, t.strftime("%A"))
+    x += 150 + 25
+painter.drawImage(x, 0, anno)
+painter.end()
+
+image.save("4-day-summary.png")
--- examples/met.no/ROMS_Model_Areas_Figure.py
+++ examples/met.no/ROMS_Model_Areas_Figure.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
 
     b = bdiana.BDiana(log_level = 5)
     b.setup()
+    b.addModel("Arctic-20km.24h_avg", "http://thredds.met.no/thredds/dodsC/sea/arctic20km/24h/aggregate_be", clear = True)
+    b.addModel("Nordic-4km.24h_avg", "http://thredds.met.no/thredds/dodsC/sea/nordic4km/24h/aggregate_be")
+    b.addModel("NorKyst-800m.24h_avg", "http://thredds.met.no/thredds/dodsC/sea/norkyst800m/24h/aggregate_be")
     
     m = plotcommands.Map(map = "Gshhs-Auto", backcolour = "white", land = "on")
     m.setOption("land.colour", "gray")
@@ -25,7 +28,7 @@ if __name__ == "__main__":
                             antialiasing = True)
     f1.setOption("line.interval", 500)
     
-    f2 = plotcommands.Field(model = "Nordic-4km24h.avg",
+    f2 = plotcommands.Field(model = "Nordic-4km.24h_avg",
                             plot = "BOTTOM_DEPTH",
                             colour = "darkcyan",
                             plottype = "contour",
@@ -34,7 +37,7 @@ if __name__ == "__main__":
                             antialiasing = True)
     f2.setOption("line.interval", 500)
     
-    f3 = plotcommands.Field(model = "NorKyst-800m.24havg",
+    f3 = plotcommands.Field(model = "NorKyst-800m.24h_avg",
                             plot = "BOTTOM_DEPTH",
                             colour = "darkblue",
                             plottype = "contour",
@@ -48,5 +51,5 @@ if __name__ == "__main__":
     b.setPlotCommands([m, f1, f2, f3, l1])
     b.setPlotTime(datetime.datetime.now())
     
-    b.plotImage(210 * 2, 210 * 2).save("/tmp/ROMS-model-areas.png")
-    b.plotPDF(210, 210, "/tmp/ROMS-model-areas.pdf", units = "mm")
+    b.plotImage(210 * 2, 210 * 2).save("ROMS-model-areas.png")
+    b.plotPDF(210, 210, "ROMS-model-areas.pdf", units = "mm")
